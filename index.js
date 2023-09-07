a3c8e33eec9d1df2bab9a95cefc2fbbcbcac2487
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-07 19:46:57 +0100

    Reading the contents of an element

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ document.getElementById('secondParagraph').innerHTML = 'HELLO from <b>Outer spac
 
 document.getElementById('mainParagraph').innerText = 'Hello from <b>Outer space</b>'
 
+const thirdParagraphContents = document.getElementById('thirdParagraph').innerText
+
+console.log(thirdParagraphContents)
+
 for (let i = 0; i < 50; i++) {
   document.write(i)
 }
