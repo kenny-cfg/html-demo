40f6ae8c51282a34c81d6ac9b2f558163a4bb8d0
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-07 20:11:24 +0100

    Create elements

--- form.js
+++ form.js
@@ -1,9 +1,12 @@
 const buttonElement = document.getElementById('button')
 const inputElement = document.getElementById('inputBox')
+const listElement = document.getElementById('list')
 
 buttonElement.addEventListener('click', () => {
   const nameEntered = inputElement.value
-  console.log(nameEntered)
+  const newListItem = document.createElement('li')
+  newListItem.innerText = nameEntered
+  listElement.append(newListItem)
 })
 
 // buttonElement.onclick = () => {
