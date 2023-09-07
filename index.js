18006bbdb8505672b6e79ebf4d00cf3f272fe4a4
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-07 19:45:27 +0100

    Change innerHTML and innerText on an element, and also document.write to append to a document

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('HELLO from index.js')
+
+document.getElementById('secondParagraph').innerHTML = 'HELLO from <b>Outer space</b>, yo!'
+
+document.getElementById('mainParagraph').innerText = 'Hello from <b>Outer space</b>'
+
+for (let i = 0; i < 50; i++) {
+  document.write(i)
+}
+document.write('HELLO')
